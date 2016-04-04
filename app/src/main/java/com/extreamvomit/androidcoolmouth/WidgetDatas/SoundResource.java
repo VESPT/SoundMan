6c4725e1e91fccb759ead6b6ae3a3d8785a8b92f
VESPT <vesp1989@gmail.com>
2016-04-05 03:06:27 +0900

    とりあえず一段落

--- app/src/main/java/com/extreamvomit/androidcoolmouth/WidgetDatas/SoundResource.java
+++ app/src/main/java/com/extreamvomit/androidcoolmouth/WidgetDatas/SoundResource.java
@@ -21,22 +21,40 @@ public class SoundResource {
     }
 
     private void SetResource() {
-        sRArray.add(R.raw.gundam01);    //01
-        sRArray.add(R.raw.gundam02);    //02
-        sRArray.add(R.raw.gundam03);    //03
-        sRArray.add(R.raw.gundam04);    //04
-        sRArray.add(R.raw.gundam05);    //05
-        sRArray.add(R.raw.zaku01);      //06
-        sRArray.add(R.raw.zaku02);      //07
-        sRArray.add(R.raw.se_maoudamashii_system46);    //08
-        sRArray.add(R.raw.kirameki01);     //9
-        sRArray.add(R.raw.takara2);     //10
-        sRArray.add(R.raw.z_ohayo);     //11
-        sRArray.add(R.raw.z_onakasuitayo);    //12
-        sRArray.add(R.raw.z_tetsuya);   //13
-        sRArray.add(R.raw.murai1);      //14
-        sRArray.add(R.raw.murai3);      //15
-        sRArray.add(R.raw.z_katsuo);    //16
+
+        sRArray.add(R.raw.se_jankenpon);    //01
+        sRArray.add(R.raw.se_aiko);    //02
+        sRArray.add(R.raw.se_yahoo);    //03
+        sRArray.add(R.raw.se_arigatou);    //04
+        sRArray.add(R.raw.se_gomennasai);    //05
+        sRArray.add(R.raw.se_kyaa);      //06
+        sRArray.add(R.raw.se_true);      //07
+        sRArray.add(R.raw.se_false);    //08
+        sRArray.add(R.raw.takara2);     //9
+        sRArray.add(R.raw.se_force);     //10
+        sRArray.add(R.raw.se_coin);     //11
+        sRArray.add(R.raw.se_slime);    //12
+        sRArray.add(R.raw.se_eeee_oozei);   //13
+        sRArray.add(R.raw.se_horror);      //14
+        sRArray.add(R.raw.se_ikemen);      //15
+        sRArray.add(R.raw.se_waaa_oozei);    //16
+
+        //sRArray.add(R.raw.gundam01);    //01
+        //sRArray.add(R.raw.gundam02);    //02
+        //sRArray.add(R.raw.gundam03);    //03
+        //sRArray.add(R.raw.gundam04);    //04
+        //sRArray.add(R.raw.gundam05);    //05
+        //sRArray.add(R.raw.zaku01);      //06
+        //sRArray.add(R.raw.zaku02);      //07
+        //sRArray.add(R.raw.se_maoudamashii_system46);    //08
+        //sRArray.add(R.raw.kirameki01);     //9
+        //sRArray.add(R.raw.takara2);     //10
+        //sRArray.add(R.raw.z_ohayo);     //11
+        //sRArray.add(R.raw.z_onakasuitayo);    //12
+        //sRArray.add(R.raw.z_tetsuya);   //13
+        //sRArray.add(R.raw.kirameki02);      //14
+        //sRArray.add(R.raw.kirameki03);      //15
+        //sRArray.add(R.raw.z_katsuo);    //16
     }
 
     public int getSoundResource(int i){
